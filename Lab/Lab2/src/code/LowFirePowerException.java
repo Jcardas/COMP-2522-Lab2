615a2d7113602c313b67702ced74a0ff7ad224fa
Justin Cardas <justincardas@gmail.com>
2025-01-17 14:55:51 -0800

    Initial Commit

---
+++ Lab/Lab2/src/code/LowFirePowerException.java
@@ -0,0 +1,7 @@
+public class LowFirePowerException extends RuntimeException
+{
+    public LowFirePowerException(String message)
+    {
+        super(message);
+    }
+}
---
+++ Lab/Lab2/src/code/LowManaException.java
@@ -0,0 +1,7 @@
+public class LowManaException extends RuntimeException
+{
+    public LowManaException(String message)
+    {
+        super(message);
+    }
+}
