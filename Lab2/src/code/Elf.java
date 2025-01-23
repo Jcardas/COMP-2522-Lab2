93fcdac5bd7dfa645858e5f3256e99b0021fe41a
Armaan <abrar158@my.bcit.ca>
2025-01-23 10:35:43 -0800

    Commented Elf class

--- Lab2/src/code/Elf.java
+++ Lab2/src/code/Elf.java
@@ -23,11 +23,23 @@ public class Elf extends Creature
 
     private int mana;
 
+    /**
+     * <p>Constructs a new {@code Elf} object with the specified
+     * {@code name}, {@code dateOfBirth}, {@code health}, and {@code mana}</p>
+     *
+     * Validates input parameter and throws an exception if parameters are invalid.
+     *
+     * @param name          the name of the creature
+     * @param dateOfBirth   Date object for the creature's date of birth
+     * @param health        The initial health of the creature
+     * @param mana          The initial mana of the creature
+     * @throws IllegalArgumentException if {@code mana} is negative or greater than {@code MAX_MANA}
+     */
     public Elf(String name,
                Date dateOfBirth,
                int health,
                int mana)
-            //Throws IllegalArgumentException if parameters are invalid
+               //Throws IllegalArgumentException if parameters are invalid.
                throws IllegalArgumentException
     {
         super(name, dateOfBirth, health);
@@ -36,6 +48,10 @@ public class Elf extends Creature
         this.mana = mana;
     }
 
+    /*
+     * Checks if {@code mana} is less than {@code MIN_MANA} or greater than
+     * {@code MAX_MANA}, if either is true, throw exception.
+     */
     private static void validateMana(final int mana)
     {
         if(mana < MIN_MANA || mana > MAX_MANA)
@@ -44,6 +60,10 @@ public class Elf extends Creature
         }
     }
 
+    /**
+     * Overrides {@code getDetails} from parent class and adds the creature's
+     * {@code mana} amount to the output.
+     */
     @Override
     void getDetails()
     {
@@ -55,8 +75,15 @@ public class Elf extends Creature
         details.append(" and has ");
         details.append(this.mana);
         details.append(" mana left.");
+
+        System.out.println(details.toString());
     }
 
+    /*
+     * <p>Reduces {@code mana} by {@code MANA_SPELL_COST} and deals {@code SPELL_POWER}
+     * amount of damage to a creature. If {@code mana} is less than {@code MANA_SPELL_COST}
+     * throw checked exception {@link LowManaException}.
+     */
     private void castSpell(Creature creatureHit)
     {
         if(creatureHit == null)
@@ -71,12 +98,19 @@ public class Elf extends Creature
         creatureHit.takeDamage( SPELL_POWER );
     }
 
+    /*
+     * Restores mana based on amount provided. Amount cannot be negative or
+     * exceed {@code MAX_MANA}
+     */
     private void restoreMana(int amount)
     {
         if(amount < MIN_MANA || amount > MAX_MANA)
         {
             throw new IllegalArgumentException("Invalid mana amount: " + amount);
         }
+
+        // Adds the mana amount to current mana,
+        // or sets the mana to MAX_MANA, whichever is lower.
         this.mana = Math.min(this.mana + amount, MAX_MANA);
     }
 }
