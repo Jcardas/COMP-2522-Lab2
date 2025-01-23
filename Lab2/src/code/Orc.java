c10065f02e12d44e32b21ca955440c6eaf412a9c
Armaan <abrar158@my.bcit.ca>
2025-01-23 13:09:33 -0800

    Completed getDetails method and berserk method for Orc class

--- Lab2/src/code/Orc.java
+++ Lab2/src/code/Orc.java
@@ -1,7 +1,19 @@
 public class Orc extends Creature
 {
+    private static final int RAGE_BOOST_REQUIREMENT;
+    private static final int RAGE_BOOSTED_DAMAGE;
+    private static final int MIN_RAGE;
+    private static final int RAGE_NORMAL_DAMAGE;
 
-    private final int rage;
+    private int rage;
+
+    static
+    {
+        RAGE_BOOST_REQUIREMENT = 20;
+        RAGE_BOOSTED_DAMAGE    = 30;
+        MIN_RAGE               = 5;
+        RAGE_NORMAL_DAMAGE     = 15;
+    }
 
     /**
      * <p>Constructs a new {@code Creature} object with the specified
@@ -35,6 +47,33 @@ public class Orc extends Creature
     @Override
     void getDetails()
     {
+        super.getDetails();
 
+        final StringBuilder details;
+        details = new StringBuilder();
+
+        details.append(" and has ");
+        details.append(this.rage);
+        details.append(" rage left.");
+
+        System.out.println(details.toString());
+    }
+
+    private void berserk(Creature creatureHit)
+    {
+        if(creatureHit == null)
+        {
+            throw new IllegalArgumentException("creatureHit cannot be null");
+        }
+        if(rage < RAGE_BOOST_REQUIREMENT)
+        {
+            creatureHit.takeDamage(RAGE_BOOSTED_DAMAGE );
+        }
+        if(rage < MIN_RAGE)
+        {
+            throw new LowRageException("Invalid rage amount: " + rage);
+        }
+        this.rage += MIN_RAGE;
+        creatureHit.takeDamage(RAGE_NORMAL_DAMAGE);
     }
 }
