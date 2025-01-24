5cd3f157e7d56add9f3da2efb014a2adc04108b0
Jcardas <justincardas@gmail.com>
2025-01-24 01:42:14 -0800

    To-Do List in Main.java

--- Lab2/src/code/main.java
+++ Lab2/src/code/main.java
@@ -3,6 +3,20 @@ public class main
     public static void main(final String[] args)
     {
 
+        /*
+         TO DO:
+
+        Unchecked Exceptions:
+        - DamageException: Thrown when an invalid damage amount is applied.
+        - HealingException: Thrown when an invalid healing amount is applied.
+        - LowRageException: Thrown when an Orc cannot go berserk due to insufficient rage.
+
+        Checked Exceptions:
+        - LowFirePowerException: Thrown when a Dragon tries to breathe fire with insufficient firePower.
+        - LowManaException: Thrown when an Elf tries to cast a spell with insufficient mana.
+         */
+
+
         //Testing
 
         Date d1 = new Date(15, 7, 2004);
